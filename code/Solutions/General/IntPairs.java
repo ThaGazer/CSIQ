212dba91e85496aecb33b207368085b1b23d1c5e
Justin Ritter <hi5ritter@hotmail.com>
2019-05-07 03:26:22 -0500

    created IntPairs.java

---
+++ code/Solutions/General/IntPairs.java
@@ -0,0 +1,26 @@
+/*
+ * Author: ThaGazer
+ * File:Solutions/ null.java
+ * Date: 5/6/2019
+ */
+package Solutions.General;
+
+import Solutions.Question;
+
+public class IntPairs extends Question {
+
+  public IntPairs() {
+    this.question = "Find pairs in an integer array whose sum is equal to 10 " +
+            "(bonus: do it in linear time)";
+  }
+
+  @Override
+  public String getQuestion() {
+    return this.question;
+  }
+
+  @Override
+  public void solve() {
+    System.out.println("");
+  }
+}
