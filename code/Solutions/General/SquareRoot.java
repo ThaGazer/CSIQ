aa989b94d77211da5a0295f574aa7d25f8e5006a
Justin Ritter <hi5ritter@hotmail.com>
2019-06-26 12:47:54 -0500

    fixed handling negative numbers

--- code/Solutions/General/SquareRoot.java
+++ code/Solutions/General/SquareRoot.java
@@ -11,6 +11,9 @@ import java.util.Random;
 
 public class SquareRoot extends Question {
 
+  private static final String errNonNegative =
+          "Can not find the square root of a negative number";
+
   public SquareRoot() {
     this.question = "Implement square root function";
   }
@@ -21,12 +24,19 @@ public class SquareRoot extends Question {
 
     System.out.println("The square root of " + x + " is: ");
 
-    System.out.println(squareRoot(x));
+    try {
+      System.out.println(squareRoot(x));
+    } catch(Exception e) {
+      System.err.println(e.getMessage());
+    }
   }
 
-  private double squareRoot(double x) {
+  private double squareRoot(double x) throws Exception {
     if(x == 0) {
       return 0;
+    }
+    if (x < 0) {
+      throw new Exception(errNonNegative);
     }
     double l = 0.0, r = 1.0;
     while(ce(l,r)) {
