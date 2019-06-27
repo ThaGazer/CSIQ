f70a45f4f29905a32cf4de9041faac7fde2635d1
Justin Ritter <hi5ritter@hotmail.com>
2019-06-27 18:25:27 -0500

    created Int2Binary

---
+++ code/Solutions/General/Int2Binary.java
@@ -0,0 +1,13 @@
+/*
+ * Author: ThaGazer
+ * File:Solutions.General/ null.java
+ * Date: 6/26/2019
+ */
+package Solutions.General;
+
+import Solutions.Question;
+
+//TODO finish
+public class Int2Binary extends Question {
+
+}
